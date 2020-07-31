7b655c9aaaa50c7d7a78eef8d855ea86be97560c
alexfaucheux <44651995+alexfaucheux@users.noreply.github.com>
2020-07-31 13:37:52 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DMGForm from './components/DMGForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Damage Calculator">
+        <DMGForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
